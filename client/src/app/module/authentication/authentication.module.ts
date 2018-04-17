7a21be91ff69a85d1ed9be16fded8f19fca562a4
cylof22 <cylof22@gmail.com>
2018-04-17 17:33:15 +0800

    add configuration for authentication service

---
+++ client/src/app/module/authentication/authentication.module.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
+import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component';
+import { AuthenticationService, AUTHETICATION_SERVICE_URL } from "./services/authentication.service"
+
+@NgModule({
+    declarations: [
+        LoginComponent,
+        RegisterComponent,
+    ],
+
+    exports: [
+        LoginComponent,
+        RegisterComponent,
+    ],
+
+    imports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpModule,
+        RouterModule.forChild([
+            {path: 'login', component: LoginComponent },
+            {path: 'register', component: RegisterComponent}
+        ])
+    ],
+
+    providers:[
+        AuthenticationService,
+        {provide: AUTHETICATION_SERVICE_URL, useValue: "http://localhost:8000/api/"},
+    ],
+})
+
+
+export class AuthenticationModule { }
