d6cd1bb90278aa6cce08b097e890613544ed2379
cylof22 <cylof22@gmail.com>
2018-04-25 10:30:19 +0800

    replace the 80 port with 8000

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  productionURL : "http://www.elforce.net:80",
+  productionURL : "http://www.elforce.net:8000",
   styleTransferURL : "http://www.elforce.net:9090/styleTransfer",
   aritstTransferURL : "http://www.elforce.net:9090/artistStyle",
 };
