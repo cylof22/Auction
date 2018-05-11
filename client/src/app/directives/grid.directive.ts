dab42469e5ba3466ab514c401d288d9c844a485d
cylof22 <cylof22@gmail.com>
2018-05-11 19:13:14 +0800

    add pinintest and grid function

--- client/src/app/directives/grid.directive.ts
+++ client/src/app/directives/grid.directive.ts
@@ -1,12 +1,36 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, TemplateRef, ViewContainerRef, OnInit } from '@angular/core';
 var Freewall = require("freewall")
 
 @Directive({
   selector: '[appGrid]'
 })
 export class GridDirective {
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef
+  ) {
+    let id = this.viewContainerRef.element.nativeElement.selector;
+    let queryId = "#" + id;
 
-  constructor() { 
+    var temp = "<div class='cell' style='width:{width}px; height: {height}px; background-image: url(i/photo/{index}.jpg)'></div>";
+			var w = 200, h = 200, html = '', limitItem = 49;
+			for (var i = 0; i < limitItem; ++i) {
+				html += temp.replace(/\{height\}/g, h.toString()).replace(/\{width\}/g, w.toString()).replace("{index}", (i + 1).toString());
+			}
+			$(queryId).html(html);
 
+			var wall = new Freewall(queryId);
+			wall.reset({
+				selector: '.cell',
+				animate: true,
+				cellW: 200,
+				cellH: 200,
+				onResize: function() {
+					wall.refresh();
+				}
+			});
+			wall.fitWidth();
+			// for scroll bar appear;
+      $(window).trigger("resize");
   }
 }
--- client/src/app/directives/pinintest.directive.ts
+++ client/src/app/directives/pinintest.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, ViewContainerRef, OnInit } from '@angular/core';
 
 var Freewall = require("freewall")
 
@@ -7,8 +7,23 @@ var Freewall = require("freewall")
 })
 export class PinintestDirective {
 
-  constructor() { 
-    
-  }
+  constructor( private viewContainerRef: ViewContainerRef) { 
+    let id = this.viewContainerRef.element.nativeElement.selector;
+    let queryId = "#" + id;
 
+    var wall = new Freewall(queryId);
+			wall.reset({
+				selector: '.brick',
+				animate: true,
+				cellW: 200,
+				cellH: 'auto',
+				onResize: function() {
+					wall.fitWidth();
+				}
+			});
+
+			wall.container.find('.brick img').load(function() {
+				wall.fitWidth();
+			});
+  }
 }
