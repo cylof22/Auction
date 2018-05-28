ae50a0f4d2d1f470dc666b356eb8877507181c0b
cylof22 <cylof22@gmail.com>
2018-05-28 20:02:53 +0800

    change ai server

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   productionURL : "http://www.elforce.net:80",
   productsURL: "http://www.elforce.net:80/api/products",
-  styleTransferURL : "http://www.elforce.net:9090/styleTransfer",
-  aritstTransferURL : "http://www.elforce.net:9090/artistStyle",
+  styleTransferURL : "http://www.j2o0918626.iask.in:58075/styleTransfer",
+  aritstTransferURL : "http://www.j2o0918626.iask.in:58075/artistStyle",
 };
